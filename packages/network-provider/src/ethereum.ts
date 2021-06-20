99a725d068876f0979152e4fb4bf2d903505ece8
yujuiting <yujuiting@me.com>
2021-06-20 16:54:01 +0800

    feat(ethereum): fetch from defi providers

--- packages/network-provider/src/ethereum.ts
+++ packages/network-provider/src/ethereum.ts
@@ -1,15 +1,31 @@
-import { BalanceFetcher } from '@bubble-tea/base';
-import { chain, fetchNativeTokenBalance, fetchERC20TokenBalance } from '@bubble-tea/ethereum';
+import { BalanceFetcher, DefiProvider, TokenAmount } from '@bubble-tea/base';
+import { chain, fetchNativeTokenBalance, fetchERC20TokenBalance, fetchInteractedAddresses } from '@bubble-tea/ethereum';
 import * as etherscan from '@bubble-tea/etherscan';
+import * as convexfinanceCom from '@bubble-tea/convexfinance.com';
 import findTokensFromTransactions from './find-token-from-transactions';
 
 export { chain, nativeToken } from '@bubble-tea/ethereum';
 
+const defiProviders: DefiProvider[] = [convexfinanceCom.stakeCvxCRV];
+
 export const fetchBalance: BalanceFetcher = async address => {
   const transactions = await etherscan.fetchAccountERC20TokenTransactions(address);
+  const interactedAddresses = await findInteractedAddresses(transactions);
   const tokens = await findTokensFromTransactions(chain, transactions);
-  return await Promise.all([
+  const promises: Promise<TokenAmount | TokenAmount[]>[] = [
     fetchNativeTokenBalance(address),
     ...tokens.map(token => fetchERC20TokenBalance(address, token)),
-  ]);
+  ];
+
+  for (const defiProvider of defiProviders) {
+    if (defiProvider.hasInteractedWith(interactedAddresses)) {
+      promises.push(defiProvider.fetchBalance(address));
+    }
+  }
+
+  return (await Promise.all(promises)).flat();
 };
+
+async function findInteractedAddresses(transactions: etherscan.Transaction[]) {
+  return (await Promise.all(transactions.map(transaction => fetchInteractedAddresses(transaction.hash)))).flat();
+}
